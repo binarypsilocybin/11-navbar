0fb24899eecadb327375959707b9fa28d309d4c8
binarypsilocybin <rogeriosvaldo@gmail.com>
2022-08-24 14:22:58 +0100

    1 file modified

--- src/Navbar.js
+++ src/Navbar.js
@@ -9,9 +9,26 @@ const Navbar = () => {
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} alt="logo" />
-          <button className="nav-toggle"> </button>
+          <button className="nav-toggle">
+            <FaBars />
+          </button>
         </div>
-        <div className="links-container show-container"></div>
+        <div className="links-container show-container">
+          <ul className="links">
+            <li>
+              <a href="#">home</a>
+            </li>
+            <li>
+              <a href="#">about</a>
+            </li>
+            <li>
+              <a href="#">contact</a>
+            </li>
+            <li>
+              <a href="#">products</a>
+            </li>
+          </ul>
+        </div>
         <ul className="social-icons"></ul>
       </div>
     </nav>
