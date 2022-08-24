1b95b1649f6520e383f486575b993175537416e4
binarypsilocybin <rogeriosvaldo@gmail.com>
2022-08-24 15:15:49 +0100

    3 files modified, 2 added

--- src/Navbar.js
+++ src/Navbar.js
@@ -14,22 +14,25 @@ const Navbar = () => {
           </button>
         </div>
         <div className="links-container show-container">
-          <ul className="links">
-            <li>
-              <a href="#">home</a>
-            </li>
-            <li>
-              <a href="#">about</a>
-            </li>
-            <li>
-              <a href="#">contact</a>
-            </li>
-            <li>
-              <a href="#">products</a>
-            </li>
-          </ul>
+          <ul className="links"></ul>
         </div>
-        <ul className="social-icons"></ul>
+        <ul className="social-icons">
+          <li>
+            <a href="https://www.twitter.com">
+              <FaTwitter />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.twitter.com">
+              <FaTwitter />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.twitter.com">
+              <FaTwitter />
+            </a>
+          </li>
+        </ul>
       </div>
     </nav>
   );
