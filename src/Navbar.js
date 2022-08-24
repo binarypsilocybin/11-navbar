32c1394d7581aa9ca331b3748499dec26471508d
binarypsilocybin <rogeriosvaldo@gmail.com>
2022-08-24 12:18:18 +0100

    1 file modified

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,21 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { FaBars, FaTwitter } from 'react-icons/fa';
 import { links, social } from './data';
-import logo from 'https://react-projects-11-navbar.netlify.app/static/media/logo.2bb7da65.svg';
+import logo from './logo.svg';
 
 const Navbar = () => {
-  return;
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="logo" />
+          <button className="nav-toggle"> </button>
+        </div>
+        <div className="links-container show-container"></div>
+        <ul className="social-icons"></ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
