037dc712fa75e0a0bff3c9e864de597de2da9900
binarypsilocybin <rogeriosvaldo@gmail.com>
2022-08-28 11:56:18 +0100

    2 files modified

--- src/Navbar.js
+++ src/Navbar.js
@@ -14,7 +14,16 @@ const Navbar = () => {
           </button>
         </div>
         <div className="links-container show-container">
-          <ul className="links"></ul>
+          <ul className="links">
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
         </div>
         <ul className="social-icons">
           <li>
